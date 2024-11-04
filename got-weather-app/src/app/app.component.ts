eeeaa82479f4e28e1446aa99930648b541cfbc6d
Manuel Marin <manuel.marin.824@my.csun.edu>
2024-11-04 15:27:04 -0800

    create account page working

--- got-weather-app/src/app/app.component.ts
+++ got-weather-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { RouterOutlet } from '@angular/router';
 import { WeatherComponent } from './weather.component';
 import { FooterComponent } from './footer.component';
@@ -11,6 +13,14 @@ import { HamburgerMenuComponent } from './hamburger-menu/hamburger-menu.componen
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'got-weather-app';
+export class AppComponent implements OnInit {
+  showContent = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
+      this.showContent = event.url !== '/login';
+    });
+  }
 }
--- got-weather-app/src/main.ts
+++ got-weather-app/src/main.ts
@@ -1,8 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter, Routes } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { LoginComponent } from './app/login/login.component';
+import { WeatherComponent } from './app/weather.component';
 import { provideHttpClient } from '@angular/common/http';
 
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'weather', component: WeatherComponent }
+];
+
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient()
+  ]
 }).catch(err => console.error(err));
