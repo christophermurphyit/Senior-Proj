534caa36e0a1db4583c7fa533569ac8d8997ccc8
virusdotexe99 <christophermurphyit@gmail.com>
2024-11-08 14:09:17 -0800

    Hamburger menu buttons for login and create
    Created buttons to navigate pages

--- got-weather-app/src/app/hamburger-menu/hamburger-menu.component.ts
+++ got-weather-app/src/app/hamburger-menu/hamburger-menu.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-hamburger-menu',
@@ -12,6 +14,9 @@ export class HamburgerMenuComponent {
   isMenuOpen = false;
   selectedUnit: 'Fahrenheit' | 'Celsius' = 'Fahrenheit'; // Default to Fahrenheit
 
+  constructor(private router: Router) {}
+
+
   @Output() unitChange = new EventEmitter<'Fahrenheit' | 'Celsius'>();
 
   toggleMenu() {
@@ -21,5 +26,13 @@ export class HamburgerMenuComponent {
   setUnit(unit: 'Fahrenheit' | 'Celsius') {
     this.selectedUnit = unit;
     this.unitChange.emit(this.selectedUnit); // Emit the selected unit
+  }
+  navigateToLogin() {
+    this.isMenuOpen = false; // Optional: close the menu after navigation
+    this.router.navigate(['/login']); // Navigate to the login page
+  }
+  navigateToCreate() {
+    this.isMenuOpen = false; // Optional: close the menu after navigation
+    this.router.navigate(['/create-account']); // Navigate to the login page
   }
 }
