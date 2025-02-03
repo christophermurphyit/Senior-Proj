c6d4d1dad72fae647d1fdde33fbc980ee142c7c2
virusdotexe99 <christophermurphyit@gmail.com>
2025-02-03 12:58:45 -0800

    Added windspeed

--- got-weather-app/src/app/weather.component.ts
+++ got-weather-app/src/app/weather.component.ts
@@ -27,6 +27,8 @@ export class WeatherComponent implements OnInit {
   private _selectedSignal: 'current' | '7day' | 'daily' = 'current'; // Default signal
   selectedUnit: 'Fahrenheit' | 'Celsius' = 'Fahrenheit'; // Default to Fahrenheit
   dailyForecast: any;
+  windSpeed: number | null = null;
+
 
 
   private readonly apiKey = 'd474509725247f01f4f5b322d067dd8b';
@@ -61,6 +63,8 @@ export class WeatherComponent implements OnInit {
       .subscribe({
         next: (data: any) => {
           console.log('Weather data received:', data); // Log to confirm data is received
+          this.windSpeed = Math.round(data.wind?.speed);
+          console.log('API Wind Speed (m/s):', data.wind?.speed);
           this.renderDOM(data);
         },
         error: () => alert('Error fetching weather data. Please try again later.'),
@@ -127,6 +131,8 @@ export class WeatherComponent implements OnInit {
         .subscribe({
           next: (data: any) => {
             console.log('Current Weather Data:', data);
+            this.windSpeed = Math.round(data.wind?.speed);
+            console.log('API Wind Speed (m/s):', data.wind?.speed);
             this.renderDOM(data);
           },
           error: (err) => {
